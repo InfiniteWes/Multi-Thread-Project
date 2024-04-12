c1f06702d0f8e990c7d43f5fc247f62cad011228
InfiniteWes <wesspangler98@gmail.com>
2024-04-12 15:09:49 -0500

    update (read & write function created)

--- main.py
+++ main.py
@@ -6,53 +6,24 @@ from multiprocessing import Pool
 parser = argparse.ArgumentParser()
 
 # Adding arguments for the files to be tested
-parser.add_argument('-i', '--input', type=str, help='Input file name', required=True)
-parser.add_argument('-o', '--output', type=str, help='Output file name', required=True)
-parser.add_argument('-p', '--processes', type=int, help='Number of processes to use', required=False, default=1)
+def parse_args():
+    parser.add_argument('-i', '--input', type=str, help='Input file name', required=True)
+    parser.add_argument('-o', '--output', type=str, help='Output file name', required=True)
+    parser.add_argument('-p', '--processes', type=int, help='Number of processes to use', required=False, default=1)
+    return parser.parse_args()
 
-def main():
-    matrix = genMatrix(100, 50)
-    MAX_PROCESSES = 10
-    finalSum = 0
-
-    processPool = Pool(processes=MAX_PROCESSES)
-    poolData = list()
-
-    # A process pool allows us to create a pool of worker processes.
-    # Each process will accept
-
-    for rowNum in range(len(matrix)):
-        matrixData = [matrix, rowNum]
-        poolData.append(matrixData)
-
-        finalData = processPool.map(addMatrixRow, poolData)
-
-    del (poolData)
-
-    for num in finalData:
-        finalSum += num
-
-    print("The final sum for the matrix is %d." %finalSum)
-
-def addMatrixRow(matrixData):
-    matrix = matrixData[0]
-    rowNum = matrixData[1]
-    del (matrixData)
-    rowSum = 0
-
-    for colNum in range(len(matrix[rowNum])):
-        rowSum += matrix[rowNum][colNum]
-    
-    return rowSum
-
-def genMatrix(row, col):
-    matrix = list()
-    for i in range(row):
-        matrix.append(list())
-        for j in range(col):
-            matrix[i].append(random.randint(0, 10))
-
+def read_matrix(file_name):
+    matrix = []
+    with open(file_name, 'r') as file:
+        for line in file:
+            matrix.append([int(x) for x in line.split()])
     return matrix
 
+def write_matrix(file_name, matrix):
+    with open(file_name, 'w') as file:
+        for row in matrix:
+            file.write(' '.join([str(x) for x in row]) + '\n')
+
 if __name__ == "__main__":
-    main()
+    args = parse_args()
+    matrix = read_matrix(args.input)
